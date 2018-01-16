cd3b62376b65132adf0679c9ebef1665b12ef5d7
DavidBest <madhawk007@bk.ru>
2018-01-16 21:48:03 +0300

    Maven modified for building jar file

--- src/sample/service/RestService.java
+++ src/sample/service/RestService.java
@@ -8,17 +8,12 @@ public class RestService {
 
 
     public static void persistProduct(Product product){
-
         RestTemplate template = new RestTemplate();
-
         template.postForEntity("http://localhost:8080/management/product/create",product,Product.class);
     }
 
     public static void persistCategory(Category category){
         RestTemplate template = new RestTemplate();
-
         template.postForEntity("http://localhost:8080/management/category/create",category,Category.class);
     }
-
-
 }
